ae648c3eadfa2a9c1c0980680eda475ea810af6c
gdd-guy <214997562+Mouq1211@users.noreply.github.com>
2025-10-29 15:51:17 -0300

    c

---
+++ navmesh/Assets/Enemy.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Vector3 initialPosition;
+    Vector3 p1, p2;
+    public float radius;
+    public float speed;
+    public Vector3 currentTarget;
+
+    Vector3 moveDirection;
+
+    Transform Player;
+    public float viewRadius;
+    public float attackRadius;
+
+
+
+    public State currentState = State.Idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        initialPosition = transform.position;
+        p1 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+        p2 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+
+        currentTarget = p1;
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (currentState == State.Idle)
+        {
+            if (transform.position != currentTarget)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+            }
+            else
+            {
+                print("chegou ao destino");
+                if (currentTarget == p1)
+                {
+                    currentTarget = p2;
+                    p1 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+                }
+                else
+                {
+                    currentTarget = p1;
+                    p2 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+                }
+            }
+
+            print(Vector3.Distance(transform.position, Player.position));
+
+            if (Vector3.Distance(transform.position, Player.position) < viewRadius)
+            {
+                OnChasingEnter();
+            }
+
+
+            moveDirection = (currentTarget - transform.position).normalized;
+
+        }
+
+        if (currentState == State.Chasing)
+        {
+            currentTarget = Player.position;
+
+            if (Vector3.Distance(transform.position, Player.position) > attackRadius)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+            }
+
+
+
+
+
+        }
+    }
+
+    void OnChasingEnter()
+    {
+        currentState = State.Chasing;
+        print("mudei para o estado chasing");
+    }
+
+
+    public enum State
+    {
+        Idle,
+        Chasing
+    }
+}
---
+++ navmesh/Assets/HealthComponent.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float damage)
+    {
+        if(currentHealth - damage > 0)
+        {
+            currentHealth -= damage;
+        }
+        else
+        {
+            currentHealth = 0;
+            print($"Eu, {gameObject.name}, morri");
+
+        }
+
+    }
+}
