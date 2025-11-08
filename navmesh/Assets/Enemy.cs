0d8b7832781b0cf6127e434f8f431c8fd651641c
Mouque <henriq8383@gmail.com>
2025-11-07 22:41:24 -0300

    c

--- navmesh/Assets/Enemy.cs
+++ navmesh/Assets/Enemy.cs
@@ -23,11 +23,11 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        Player = GameObject.FindGameObjectWithTag( "Player" ).transform;
 
         initialPosition = transform.position;
-        p1 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
-        p2 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+        p1 = initialPosition + new Vector3( Random.Range( 0f, radius ), Random.Range( 0f, radius ), 0f );
+        p2 = initialPosition + new Vector3( Random.Range( 0f, radius ), Random.Range( 0f, radius ), 0f );
 
         currentTarget = p1;
     }
@@ -36,46 +36,45 @@ public class Enemy : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (currentState == State.Idle)
+        if ( currentState == State.Idle )
         {
-            if (transform.position != currentTarget)
+            if ( transform.position != currentTarget )
             {
-                transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards( transform.position, currentTarget, speed * Time.deltaTime );
             }
             else
             {
-                print("chegou ao destino");
-                if (currentTarget == p1)
+                print( "chegou ao destino" );
+                if ( currentTarget == p1 )
                 {
                     currentTarget = p2;
-                    p1 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+                    p1 = initialPosition + new Vector3( Random.Range( 0f, radius ), Random.Range( 0f, radius ), 0f );
                 }
                 else
                 {
                     currentTarget = p1;
-                    p2 = initialPosition + new Vector3(Random.Range(0f, radius), Random.Range(0f, radius), 0f);
+                    p2 = initialPosition + new Vector3( Random.Range( 0f, radius ), Random.Range( 0f, radius ), 0f );
                 }
             }
 
-            print(Vector3.Distance(transform.position, Player.position));
 
-            if (Vector3.Distance(transform.position, Player.position) < viewRadius)
+            if ( Vector3.Distance( transform.position, Player.position ) < viewRadius )
             {
                 OnChasingEnter();
             }
 
 
-            moveDirection = (currentTarget - transform.position).normalized;
+            moveDirection = ( currentTarget - transform.position ).normalized;
 
         }
 
-        if (currentState == State.Chasing)
+        if ( currentState == State.Chasing )
         {
             currentTarget = Player.position;
 
-            if (Vector3.Distance(transform.position, Player.position) > attackRadius)
+            if ( Vector3.Distance( transform.position, Player.position ) > attackRadius )
             {
-                transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards( transform.position, currentTarget, speed * Time.deltaTime );
             }
 
 
@@ -88,7 +87,7 @@ public class Enemy : MonoBehaviour
     void OnChasingEnter()
     {
         currentState = State.Chasing;
-        print("mudei para o estado chasing");
+        print( "mudei para o estado chasing" );
     }
 
 
